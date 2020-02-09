5d2f05fdc1b61c2e27a2768a4a6e812f41df8bfc
AlienDos <AlienDos@DESKTOP-31D4TA8>
2020-02-09 13:32:49 -0800

    Changed Vehicle Page with buttons and user info.

--- CSE455V2/CSE455V2/Views/VehiclePage.xaml.cs
+++ CSE455V2/CSE455V2/Views/VehiclePage.xaml.cs
@@ -12,9 +12,25 @@ namespace CSE455V2.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VehiclePage : ContentPage
     {
+        public string CarInfo = "A1B234C";
         public VehiclePage()
         {
             InitializeComponent();
+            LicensePlate.Text = CarInfo;
+        }
+
+        async void EditLicensePlate(object sender, EventArgs args)
+        {
+            string result = await DisplayPromptAsync("Edit Vehical Information", "Please enter new License Plate Information?");
+        }
+        async void DeleteLicensePlate(object sender, EventArgs args)
+        {
+            bool answer = await DisplayAlert("Delete Vehical Info", "Are you sure you want to delete this information?", "Yes", "No");
+
+            if (answer == true)
+            {
+                //Delete current UserData, otherwise output "no Info exist"
+            }
         }
     }
 }
