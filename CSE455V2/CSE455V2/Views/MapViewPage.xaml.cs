52eb438ad3b1e6353283a976fe2bb6f8f47b7740
AlienDos <AlienDos@DESKTOP-31D4TA8>
2020-02-15 19:39:41 -0800

    updating xamarin forms

--- CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
+++ CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+
 using Xamarin.Forms.Xaml;
 
 namespace CSE455V2.Views
