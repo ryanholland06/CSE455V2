eb77d539b1a0359de0c50f1f6a5e0204a9231b2a
roy <005309989@coyote.csusb.edu>
2020-02-23 00:10:43 -0800

    Added remaining parking lot green boarders

--- CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
+++ CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
@@ -35,6 +35,70 @@ namespace CSE455V2.Views
             //map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(1)));
             Debug.WriteLine("$$$$$$$$The center is: " + mapSpan.Radius.Meters + "  $$$$$");
 
+            Polygon lotF_Back = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.186291, -117.327119),
+                    new Position(34.185522, -117.326332),
+                    new Position(34.185773, -117.325967),
+                    new Position(34.185904, -117.326082),
+                    new Position(34.186079, -117.325865),
+                    new Position(34.186458, -117.326289),
+                    new Position(34.186655, -117.326045),
+                    new Position(34.186910, -117.326410)
+                }
+            };
+
+            Polygon lotA = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.186495, -117.328797),
+                    new Position(34.185785, -117.328000),
+                    new Position(34.186302, -117.327356),
+                    new Position(34.186828, -117.327976)
+                }
+            };
+
+            Polygon lotB = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.182851, -117.329707),
+                    new Position(34.183773, -117.328918),
+                    new Position(34.183513, -117.328231),
+                    new Position(34.183943, -117.328349),
+                    new Position(34.184149, -117.328700),
+                    new Position(34.184180, -117.329025)
+                }
+            };
+
+            Polygon lotB_Annex = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.184013, -117.329084),
+                    new Position(34.183249, -117.328468),
+                    new Position(34.182683, -117.328151),
+                    new Position(34.182570, -117.328237),
+                    new Position(34.182506, -117.328535),
+                    new Position(34.182459, -117.329506)
+                }
+            };
+
             Polygon lotC = new Polygon
             {
                 StrokeWidth = 8,
@@ -80,7 +144,7 @@ namespace CSE455V2.Views
                 }
             };
 
-            Polygon lotF = new Polygon
+            Polygon lotF_Front = new Polygon
             {
                 StrokeWidth = 8,
                 StrokeColor = Color.FromHex("#03C04A"),
@@ -124,12 +188,56 @@ namespace CSE455V2.Views
                 }
             };
 
+            Polygon lot_EPS = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.184554, -117.320294),
+                    new Position(34.183789, -117.320911),
+                    new Position(34.183472, -117.320176),
+                    new Position(34.184189, -117.319618)
+                }
+            };
+
+            Polygon lotN = new Polygon
+            {
+                StrokeWidth = 8,
+                StrokeColor = Color.FromHex("#03C04A"),
+                FillColor = Color.FromHex("#8803C04A"),
+                Geopath =
+                {
+                    new Position(34.184839, -117.320503),
+                    new Position(34.183932, -117.321190),
+                    new Position(34.184271, -117.321850),
+                    new Position(34.183738, -117.322287),
+                    new Position(34.184286, -117.323234),
+                    new Position(34.184643, -117.322952),
+                    new Position(34.185178, -117.323931),
+                    new Position(34.185635, -117.323606),
+                    new Position(34.185586, -117.323456),
+                    new Position(34.185843, -117.323204),
+                    new Position(34.185426, -117.322359),
+                    new Position(34.185557, -117.322230),
+                    new Position(34.185071, -117.321259),
+                    new Position(34.185184, -117.321146)
+                }
+            };
+
+            map.MapElements.Add(lotF_Back);
+            map.MapElements.Add(lotA);
+            map.MapElements.Add(lotB_Annex);
+            map.MapElements.Add(lotB);
             map.MapElements.Add(lotC);
             map.MapElements.Add(lotD);
             map.MapElements.Add(lotE);
-            map.MapElements.Add(lotF);
+            map.MapElements.Add(lotF_Front);
             map.MapElements.Add(lotG);
             map.MapElements.Add(lotH);
+            map.MapElements.Add(lot_EPS);
+            map.MapElements.Add(lotN);
         }
 
     }
