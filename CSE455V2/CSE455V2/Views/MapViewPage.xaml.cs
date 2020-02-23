37b999905ee21a4bfdc12906ea108fb5e6ef9d24
AlienDos <AlienDos@DESKTOP-31D4TA8>
2020-02-22 20:06:32 -0800

    Locked map in place

--- CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
+++ CSE455V2/CSE455V2/Views/MapViewPage.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
-
 using Xamarin.Forms.Xaml;
 
 namespace CSE455V2.Views
@@ -14,19 +13,27 @@ namespace CSE455V2.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapViewPage : ContentPage
     {
+        Position position = new Position(34.182821, -117.323526);
+        MapSpan mapSpan;
+        Map map;
         public MapViewPage()
         {
             InitializeComponent();
 
-            Position position = new Position(34.182821, -117.323526);
-            MapSpan mapSpan = new MapSpan(position, 0.01, 0.01);
+            mapSpan = new MapSpan(position, 0.01, 0.01);
+            //mapSpan.Radius.Meters.Equals(300);
 
-            Map map = new Map(mapSpan)     // default can just be: Map map = new Map();
+            map = new Map(mapSpan)     // default can just be: Map map = new Map();
             {
-                MapType = MapType.Street    // can change between views
+                MapType = MapType.Street,   // can change between views
+                HasScrollEnabled = false,
+                HasZoomEnabled = false
             };
 
             Content = map;
+
+            //map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(1)));
+            Debug.WriteLine("$$$$$$$$The center is: " + mapSpan.Radius.Meters + "  $$$$$");
         }
     }
 }
