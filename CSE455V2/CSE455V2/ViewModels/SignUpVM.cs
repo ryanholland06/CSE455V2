77ddafc177f4b8c1afb03f41e57c5e977b2d5e26
roy <005309989@coyote.csusb.edu>
2020-02-29 04:13:57 -0800

    Changed Signup UI and added loading indicator

--- CSE455V2/CSE455V2/ViewModels/SignUpVM.cs
+++ CSE455V2/CSE455V2/ViewModels/SignUpVM.cs
@@ -12,6 +12,30 @@ namespace CSE455V2.ViewModel
 {
     public class SignUpVM : INotifyPropertyChanged
     {
+
+        private bool signenable = true;
+
+        public bool SignEnable
+        {
+            get { return signenable; }
+            set 
+            {
+                signenable = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SignEnable"));
+            }
+        }
+
+        private bool truefalse = false;
+        public bool Truefalse
+        {
+            get { return truefalse; }
+            set
+            {
+                truefalse = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Truefalse"));
+            }
+        }
+
         private string email;
         public string Email
         {
@@ -142,38 +166,55 @@ namespace CSE455V2.ViewModel
                 return new Command(() =>
                 {
                     var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
-
+                    Truefalse = true;
+                    SignEnable = false;
                     //null or empty field validation, check weather email and password is null or empty
                     if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
                     }
                     else if (!Email.Contains("@"))
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Email Address is Invalid", "OK");
                     }
                     else if (Password.Length < 8)
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Password is less than 8 characters", "OK");
                     }
                     else if (!Password.Any(char.IsUpper))
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Password Must Contain at Least 1 Uppcase Letter", "OK");
                     }
                     else if (!hasSymbols.IsMatch(Password))
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Password should contain At least one special case characters", "OK");
                     }
                     else if (Password != ConfirmPassword)
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Password must be same as above!", "OK");
                     }
                     else if (string.IsNullOrEmpty(StudentID))
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Student ID", "OK");
                     }
                     else if (StudentID.Length < 9)
                     {
+                        Truefalse = false;
+                        SignEnable = true;
                         App.Current.MainPage.DisplayAlert("", "Student ID is Incorrect!", "OK");
                     }
                     else
@@ -194,13 +235,20 @@ namespace CSE455V2.ViewModel
             //AddUser return true if data insert successfuly 
             if (user)
             {
+
                 await App.Current.MainPage.DisplayAlert("SignUp Success", "", "Ok");
                 //Navigate to Wellcom page after successfuly SignUp
                 //pass user email to welcom page
+                SignEnable = true;
+                Truefalse = false;
                 await App.Current.MainPage.Navigation.PushAsync(new LoginPage());
             }
             else
+            {
+                SignEnable = true;
+                Truefalse = false;
                 await App.Current.MainPage.DisplayAlert("Error", "SignUp Fail", "OK");
+            }
         }
     }
 }
